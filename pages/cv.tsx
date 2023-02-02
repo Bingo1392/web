d633a59f0506b3e4a154b43fe5f6e98b429405db
Jan Žitník <mail@janzitnik.com>
2023-02-02 23:13:48 +0100

    Fixes: Mobile menu, cv docx template

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,3 +1,22 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  myDiv: {
+    backgroundColor: "blue",
+
+    [`@media print`]: {
+      backgroundColor: "red",
+    },
+  },
+
+  [`@media print`]: {
+    myDiv: {
+      backgroundColor: "red",
+    },
+  },
+}));
+
 export default function Cv() {
-  return <div>CV</div>;
+  const { classes } = useStyles();
+  return <div className={classes.myDiv}>This is my CV</div>;
 }
